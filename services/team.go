d3636889c7e288b8f2990925556d8d7bca9ab686
Santiago-Balcero <santiagobalcero@gmail.com>
2023-06-14 15:38:45 -0500

    feat: team names confirmation before game

--- services/team.go
+++ services/team.go
@@ -13,11 +13,25 @@ type Team struct {
 func CreateTeam() Team {
 	var teamName string
 	fmt.Println()
-	for len(teamName) != 3 {
-		fmt.Print("Enter team's name: ")
-		fmt.Scan(&teamName)
-		teamName = strings.TrimSpace(teamName)
-		teamName = strings.ToUpper(teamName)
+	for {
+		for len(teamName) != 3 {
+			fmt.Print("Enter team's name: ")
+			fmt.Scan(&teamName)
+			teamName = strings.TrimSpace(teamName)
+			teamName = strings.ToUpper(teamName)
+		}
+		var confirm string
+		for confirm != "y" && confirm != "n" {
+			fmt.Printf("Confirm name %s? [y/n]: ", teamName)
+			fmt.Scan(&confirm)
+		}
+		if confirm == "y" {
+			break
+		} else if confirm == "n" {
+			teamName = ""
+			fmt.Println()
+			continue
+		}
 	}
 	return Team{Name: teamName}
 }
@@ -25,11 +39,25 @@ func CreateTeam() Team {
 func (t *Team) StartGame() Game {
 	var opponent string
 	fmt.Println()
-	for len(opponent) != 3 || opponent == t.Name {
-		fmt.Print("Enter opponent team's name: ")
-		fmt.Scan(&opponent)
-		opponent = strings.TrimSpace(opponent)
-		opponent = strings.ToUpper(opponent)
+	for {
+		for len(opponent) != 3 || opponent == t.Name {
+			fmt.Print("Enter opponent team's name: ")
+			fmt.Scan(&opponent)
+			opponent = strings.TrimSpace(opponent)
+			opponent = strings.ToUpper(opponent)
+		}
+		var confirm string
+		for confirm != "y" && confirm != "n" {
+			fmt.Printf("Confirm opponent team's name %s? [y/n]: ", opponent)
+			fmt.Scan(&confirm)
+		}
+		if confirm == "y" {
+			break
+		} else if confirm == "n" {
+			opponent = ""
+			fmt.Println()
+			continue
+		}
 	}
 	return Game{
 		Team:     t.Name,
